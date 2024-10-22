5acc0b99e5a359a894666b64d9b4afb022123c7b
Anthony Piegaro <84252391+anthonypiegaro@users.noreply.github.com>
2024-10-22 19:19:12 +0000

    Add privacy policy page

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -1,5 +1,4 @@
 import Privacy from "@/markdown/privacy.mdx";
-import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 
 export default function Page() {
