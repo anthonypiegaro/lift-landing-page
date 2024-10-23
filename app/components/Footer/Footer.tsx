b989a2570453eb28059a880b2f829d912fa087d5
Anthony Piegaro <84252391+anthonypiegaro@users.noreply.github.com>
2024-10-23 02:34:11 +0000

    Add socials to footer

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -17,12 +17,24 @@ export default function Footer() {
                 </nav>
             </div>
             <div className="flex flex-row justify-center pb-10">
-                <FaInstagramSquare size={32} className="p-1"/>
-                <FaSquareXTwitter size={32} className="p-1"/>
-                <FaLinkedin size={32} className="p-1"/>
-                <AiFillTikTok size={32} className="p-1"/>
-                <FaYoutubeSquare size={32} className="p-1"/>
-                <FaFacebookSquare size={32}  className="p-1"/>
+                <a href="https://www.instagram.com/liftlabs.dev?igsh=MzRlODBiNWFlZA==">
+                    <FaInstagramSquare size={32} className="p-1"/>
+                </a>
+                <a href="https://x.com/liftlabsdev?s=21">
+                    <FaSquareXTwitter size={32} className="p-1"/>
+                </a>
+                <a href="https://www.linkedin.com/in/lift-labs-271812334?utm_source=share&utm_medium=member_mweb&utm_campaign=share_via&utm_content=profile">
+                    <FaLinkedin size={32} className="p-1"/>
+                </a>
+                <a href="https://www.tiktok.com/@lift.labs?_t=8qkILxukq1m&_r=1">
+                    <AiFillTikTok size={32} className="p-1"/>
+                </a>
+                <a href="https://youtube.com/@liftlabsdev?si=Ds6Fssn34RFj5gCs">
+                    <FaYoutubeSquare size={32} className="p-1"/>
+                </a>
+                <a href="https://www.facebook.com/share/1eS23GenSZMwdCLW/?mibextid=LQQJ4d">
+                    <FaFacebookSquare size={32}  className="p-1"/>
+                </a>
             </div>
             <p>&copy; 2024 lift labs, Inc. All rights reserved.</p>
         </footer>
